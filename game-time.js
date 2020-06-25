f3ed9f90c5f78a9545abe9d88900e919ccedae8b
Mary Grace Bunch <marygrace@Marys-MacBook-Pro-2.local>
2020-06-24 21:26:48 -0400

    plant problems

---
+++ game-time.js
@@ -0,0 +1,44 @@
+let level;
+let growth;
+let gameOver;
+let waterNeed;
+
+$('.start-button').on('click', function() {
+    level = 1;
+    growth = [0,0,0,0];
+    gameOver = false;
+    waterNeed = [false, false, false, false];
+
+    while (!gameOver){
+        simulateGame();
+        break;
+    }
+});
+
+function simulateGame(){
+
+    let index = level-1; // the index we are primarily concerned with growing. 
+    // keep going until the plant has grown to stage 4. 
+    while (growth[index] < 4){
+        grow(index);
+    }
+}
+
+function grow(plantIndex){
+    elem = ".pot-" + plantIndex + ".leaves.level-" + level;
+    growth[plantIndex]++;
+    console.log(elem);
+    $(elem).show();
+}
+
+function needsWater(plantIndex){
+    elem = '.pot' + plantIndex + ".leaves";
+    $(elem).addClass(".dying");
+    waterNeed[plantIndex] = true;
+}
+
+function receiveWater(plantIndex){
+    elem = '.pot' + plantIndex + ".leaves";
+    $(elem).removeClass(".dying");
+}
+
